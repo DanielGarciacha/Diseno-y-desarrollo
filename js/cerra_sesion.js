703ce3cce585fa406af071f8becbfdb12f6fb24f
DanielGarciacha <130863675+DanielGarciacha@users.noreply.github.com>
2026-07-05 15:38:12 -0500

    Delete js directory

--- js/cerra_sesion.js
+++
@@ -1,9 +0,0 @@
-function cerrarSesion() {
-    // Elimina toda la información de sessionStorage y localStorage
-    sessionStorage.clear();
-    localStorage.clear();
-  
-    // Redirige a la página de login y evita volver atrás
-    location.replace("login.html");
-  }
-  
